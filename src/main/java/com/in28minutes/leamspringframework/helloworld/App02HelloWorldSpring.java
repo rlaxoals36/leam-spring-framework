97e644e90f4e7848822b5544f6f0cf5c87a6378c
TaeMin Kim <gimtaemin36@gmail.com>
2023-10-29 19:41:48 +0900

    @Bean- 기본 및 한정자

---
+++ src/main/java/com/in28minutes/leamspringframework/helloworld/App02HelloWorldSpring.java
@@ -0,0 +1,21 @@
+package com.in28minutes.leamspringframework.helloworld;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring {
+    public static void main(String[] args) {
+
+        try(var context =
+                    new AnnotationConfigApplicationContext(HelloWorldConfiguration.class)) {
+            System.out.println(context.getBean("name"));
+            System.out.println(context.getBean("age"));
+            System.out.println(context.getBean("person"));
+            System.out.println(context.getBean("person2MethodCall"));
+            System.out.println(context.getBean("person3Parameters"));
+            System.out.println(context.getBean("address2"));
+            System.out.println(context.getBean(Person.class));
+            System.out.println(context.getBean(Address.class));
+        }
+
+    }
+}
